775e551d589d10b9af9688d3f786dcd6e5939b68
Lilia Sviridova <svirivoda@gmail.com>
2022-04-26 19:15:55 +0300

    Open and close popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
-console.log("Im here");
+const popup = document.querySelector(".popup");
+const openPopup = document.querySelector(".popup-open");
+const closePopup = document.querySelector(".popup__close-button");
+
+openPopup.addEventListener("click", function () {
+  popup.classList.add("popup_opened");
+});
+
+closePopup.addEventListener("click", function () {
+  popup.classList.remove("popup_opened");
+});
