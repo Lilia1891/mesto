d223f59c462d02c679d3efb4d8b150e5c47794ac
Lilia Sviridova <svirivoda@gmail.com>
2022-04-30 10:51:00 +0300

    Fix review errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,11 @@
 const popup = document.querySelector(".popup");
-const openPopup = document.querySelector(".popup-open");
+const openPopup = document.querySelector(".profile__avatar-edit-button");
 const closePopup = document.querySelector(".popup__close-button");
 const formElement = document.querySelector(".popup__container");
-const nameInput = document.querySelector(".popup__name");
-const jobInput = document.querySelector(".popup__occupation");
+const nameInput = document.querySelector(".popup__input_type_name");
+const jobInput = document.querySelector(".popup__input_type_job");
 const nameProfile = document.querySelector(".profile__info-name");
 const jobProfile = document.querySelector(".profile__info-occupation");
-
-openPopup.addEventListener("click", function () {
-  popup.classList.add("popup_opened");
-});
-
-closePopup.addEventListener("click", function () {
-  popup.classList.remove("popup_opened");
-});
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -22,5 +14,19 @@ function formSubmitHandler(evt) {
   nameProfile.textContent = nameValue;
   jobProfile.textContent = jobValue;
 }
+function openPopupHandler(evt) {
+  popup.classList.add("popup_opened");
+  const nameValue = nameProfile.textContent;
+  nameInput.value = nameValue;
+  const jobValue = jobProfile.textContent;
+  jobInput.value = jobValue;
+}
+
+openPopup.addEventListener("click", openPopupHandler);
+
+function closePopupHandler(evt) {
+  popup.classList.remove("popup_opened");
+}
+closePopup.addEventListener("click", closePopupHandler);
 
 formElement.addEventListener("submit", formSubmitHandler);
